bb378ff2f475d5346f990de9eed0bb1780d5565e
Aisyah_Nur_Imanah <2301921@sit.singaporetech.edu.sg>
2025-07-25 16:46:21 +0800

    Updated 4

--- test_ui.py
+++ test_ui.py
@@ -1,9 +1,7 @@
-# test_ui.py
-
 import requests
 from bs4 import BeautifulSoup
 
-resp = requests.get('http://127.0.0.1')
+resp = requests.get('http://127.0.0.1:5050')  # ✅ Specify correct port
 soup = BeautifulSoup(resp.text, 'html.parser')
 assert soup.find('form'), 'Form not found!'
 print('✅ UI test passed!')
